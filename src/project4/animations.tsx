2c77724a87ff2e18526ef0929f26169e2f545e49
Your Name <ugwucollins027@gmail.com>
2023-11-14 13:01:35 +0100

    done
    done

---
+++ src/project4/animations.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const animations = () => {
+  return (
+    <div className="animation">
+      <div className="well">
+      <button className="btn">Welcome Dear</button>
+      </div>
+      
+      <button className="called">called</button>
+    </div>
+  );
+};
+
+export default animations;
